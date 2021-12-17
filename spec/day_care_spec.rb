0c71b70d9a7f3c0f4b9b309beea47ba6db4b9827
Chris Simmons <chris@turing.edu>
2021-12-17 11:40:37 -0500

    refactor: day_care_spec test structure

--- spec/day_care_spec.rb
+++ spec/day_care_spec.rb
@@ -7,55 +7,57 @@ RSpec.describe DayCare do
     @day_care = DayCare.new("The Dog Spot")
   end
 
-  it "exists" do
-    expect(@day_care).to be_instance_of(DayCare)
+  describe "Object" do
+    it "exists" do
+      expect(day_care).to be_instance_of(DayCare)
+    end
+
+    it "has a name" do
+      expect(@day_care.name).to eq("The Dog Spot")
+    end
+
+    it "starts with no customers" do
+      expect(@day_care.customers).to eq([])
+    end
   end
 
-  it "has a name" do
-    expect(@day_care.name).to eq("The Dog Spot")
-  end
+  describe "Integrations with Pets and Customers" do
+    before(:each) do
+      @joel = Customer.new("Joel", 2)
+      @billy = Customer.new("Billy", 3)
+      @samson = Pet.new({name: "Samson", type: :dog})
+      @lucy = Pet.new({name: "Lucy", type: :cat})
+      @molly = Pet.new({name: "Molly", type: :cat})
+    end
 
-  it "starts with no customers" do
-    expect(@day_care.customers).to eq([])
-  end
+    it "can add customers" do
+      @day_care.add_customer(@joel)
+      @day_care.add_customer(@billy)
+      expect(@day_care.customers).to eq([@joel, @billy])
+    end
 
-  it "can add customers" do
-    joel = Customer.new("Joel", 2)
-    billy = Customer.new("Billy", 3)
+    it "can list unfed pets" do
+      @joel.adopt(@samson)
+      @joel.adopt(@lucy)
+      @billy.adopt(@molly)
 
-    @day_care.add_customer(joel)
-    @day_care.add_customer(billy)
-    expect(@day_care.customers).to eq([joel, billy])
-  end
+      @day_care.add_customer(@joel)
+      @day_care.add_customer(@billy)
 
-  it "can list unfed pets" do
-    joel = Customer.new("Joel", 2)
-    samson = Pet.new({name: "Samson", type: :dog})
-    lucy = Pet.new({name: "Lucy", type: :cat})
-    joel.adopt(samson)
-    joel.adopt(lucy)
-    billy = Customer.new("Billy", 3)
-    molly = Pet.new({name: "Molly", type: :cat})
-    billy.adopt(molly)
+      @lucy.feed
 
-    @day_care.add_customer(joel)
-    @day_care.add_customer(billy)
+      expect(@day_care.unfed_pets).to eq([@samson, @molly])
 
-    lucy.feed
+    end
 
-    expect(@day_care.unfed_pets).to eq([samson, molly])
+    it "can find customer by id" do
+      @joel.charge(10)
 
-  end
+      @day_care.add_customer(@joel)
+      @day_care.add_customer(@billy)
 
-  it "can find customer by id" do
-    joel = Customer.new("Joel", 2)
-    joel.charge(10)
-    billy = Customer.new("Billy", 3)
-
-    @day_care.add_customer(joel)
-    @day_care.add_customer(billy)
-
-    expect(@day_care.customer_by_id(2)).to eq(joel)
+      expect(@day_care.customer_by_id(2)).to eq(@joel)
+    end
   end
 
 end
